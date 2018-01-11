0a497cb35a67545d62687dcf82904355ce6e27ba
liangsay <sonywuyang@163.com>
2018-01-11 23:02:54 +0800

    Initial

---
+++ Example/Pods/Target Support Files/Pods-WUFrameWork_Tests/Pods-WUFrameWork_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WUFrameWork_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WUFrameWork_Tests
+@end
---
+++ Example/Pods/Target Support Files/WUFrameWork/WUFrameWork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WUFrameWork : NSObject
+@end
+@implementation PodsDummy_WUFrameWork
+@end
